898bfb04d5e91b4b8bb1c84de5a6d369a8d9482c
Gaya Héliès <91890427+guy246@users.noreply.github.com>
2022-05-09 15:04:56 +0200

    Update different_etat.py

--- different_etat.py
+++ different_etat.py
@@ -1,81 +1,82 @@
 #nombre des différents termes du tableau
 
-def etat_initial (lines):
+def etat_initial(lines):
     n = 1
-    etat_initial=[]
+    etat_ini=[]
 
     for ligne in lines:
         p = 2
         if n == 3:
-            nombre_initial=int(ligne[0])
-            for i in range (nombre_initial):
-                etat_initial.append(int(ligne[p]))
+            nbre_ini = int(ligne[0])
+            for i in range(nbre_ini):
+                etat_ini.append(int(ligne[p]))
                 p = p + 2
-            n = + 1
-        return etat_initial
+        n = + 1
+        return etat_ini
 
-def etat_final (lines):
+def etat_final(lines):
     n = 1
-    etat_final= []
+    etat_fin= []
 
     for ligne in lines:
         d = 2
         if n == 4:
-            nombre_final = int(ligne[0])
-            for i in range(nombre_final):
-                etat_final.append(int(ligne[d]))
+            nbre_fin = int(ligne[0])
+            for i in range(nbre_fin):
+                etat_fin.append(int(ligne[d]))
                 d = d + 2
-            n = n + 1
-        return etat_final
+        n = n + 1
+        return etat_fin
 
-def nombre_initial (lines):
-    n=1
-    temp=""
-    m=0
+def nombre_initial(lines):
+    n = 1
+    temp = ""
+    m = 0
     for ligne in lines:
-        if n==3:
+        if n == 3:
             while ligne[m] !=" ":
-               temp=temp+ligne[m]
-               m+=1
-        nbre_ini=temp
-        n=n+1
+               temp = temp+ligne[m]
+               m += 1
+        nbre_ini = temp
+        n = n + 1
     return nbre_ini
 
 
-def nombre_final (lines):
+def nombre_final(lines):
     n = 1
-    temp=""
-    m=0
+    temp = ""
+    m = 0
     for ligne in lines:
         if n == 4:
-            while ligne[m] !=" ":
+            while ligne[m] != " ":
                 temp = temp+ligne[m]
-                m+=1
-            nbre_final = temp
+                m += 1
+        nbre_fin = temp
         n =+ 1
 
-        return nbre_final
+        return nbre_fin
 
-def nombre_etat (lines):
+def nombre_etat(lines):
     n = 1
+    nbre_etat =0
     for ligne in lines:
         if n == 2:
-            nombre_etat = int(ligne[0])
+            nbre_etat = int(ligne[0])
         n = n + 1
+        return nbre_etat
 
-        return nombre_etat
+def nombre_epsilon(lines):
+    nbre_eps = lines[0]
+    return nbre_eps
 
-def nombre_epsilon (lines):
-    nombre_epsilon= lines[0]
-    return nombre_epsilon
-
-def nombre_lien (lines):
-    n=1
+def nombre_lien(lines):
+    n = 1
+    nbre_lien=0
     for ligne in lines:
         if n == 5:
-            nombre_lien = int(ligne)
+            nbre_lien = int(ligne)
         n = n + 1
-    return nombre_lien
+    return nbre_lien
 
 
 
